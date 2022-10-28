f45bf7c12ff355f4565b24c7e786f7d56812af6d
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2022-10-28 14:35:28 +0600

    platinam-tutorial carousel added

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -3,12 +3,25 @@ import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+// import Pdf from "react-to-pdf";
+
 const Course = () => {
     const course = useLoaderData();
     // console.log(course);
 
+    // const ref = React.createRef();
+
     return (
         <div className='container shadow mt-5 mb-5 p-5 border rounded'>
+
+            {/* <h4>Download:</h4>
+            <Pdf targetRef={ref} filename="code-example.pdf">
+                {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+            </Pdf>
+            <div ref={ref}>
+                <h1>Hello CodeSandbox</h1>
+                <h2>Start editing to see some magic happen!</h2>
+            </div> */}
 
             <Card style={{ width: 'auto' }}>
                 <Card.Img variant="top" src={course.image} />
--- src/Pages/HomeCarousel/HomeCarousel.js
+++ src/Pages/HomeCarousel/HomeCarousel.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../../Images/img1.png';
+import img2 from '../../Images/img2.webp';
+import img3 from '../../Images/img3.jpg';
 
 const HomeCarousel = () => {
     return (
         <div>
             <Carousel variant="dark">
-                <Carousel.Item>
+                <Carousel.Item interval={1000}>
                     <img
                         className="d-block w-100"
-                        src="./Images/img1.png"
+                        src={img1}
                         alt="First slide"
                     />
                     <Carousel.Caption>
@@ -16,10 +19,10 @@ const HomeCarousel = () => {
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item interval={500}>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=eee"
+                        src={img2}
                         alt="Second slide"
                     />
                     <Carousel.Caption>
@@ -27,52 +30,14 @@ const HomeCarousel = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item interval={500}>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+                        src={img3}
                         alt="Third slide"
                     />
                     <Carousel.Caption>
                         <h5>Third slide label</h5>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-
-            <Carousel>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src="../../Images/img2.webp"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src="../../Images/img1.png"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="../../Images/img3.jpg"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
                         <p>
                             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                         </p>
